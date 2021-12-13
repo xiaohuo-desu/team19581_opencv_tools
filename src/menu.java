802281dc229b24497a5473b8fc4eecd5b37294b0
小火 <1178035823@qq.com>
2021-12-13 17:03:53 +0800

    - 修复opencv错误的方法使用 - 删除无意义的识别率显示 - 由LAB颜色模型改为Opencv默认的HSV - 固定maxS与maxV（无意义） - 修正部分逻辑问题

--- src/menu.java
+++ src/menu.java
@@ -64,8 +64,8 @@ public class menu extends main
                                 String str = null;
                                 if(!"e".equals(str))
                                 {
-                                    maxL -= 2;
-                                    System.out.println("现在的L是："+maxL);
+                                    maxH -= 2;
+                                    System.out.println("现在的H是："+maxH);
                                     opencv_test.opencv(input, output,0);
                                     System.out.println("按任意键继续，E停止");
                                     Scanner temp = new Scanner(System.in);
@@ -84,8 +84,8 @@ public class menu extends main
                                 String str = null;
                                 if(!"E".equals(str)||!"e".equals(str))
                                 {
-                                    maxA -= 2;
-                                    System.out.println("现在的A是："+maxA);
+                                    maxS -= 2;
+                                    System.out.println("现在的S是："+maxS);
                                     opencv_test.opencv(input, output,0);
                                     System.out.println("按任意键继续，E停止");
                                     Scanner temp = new Scanner(System.in);
@@ -104,8 +104,8 @@ public class menu extends main
                                 String str = null;
                                 if(!"E".equals(str)||!"e".equals(str))
                                 {
-                                    maxB -= 2;
-                                    System.out.println("现在的B是："+maxB);
+                                    maxV -= 2;
+                                    System.out.println("现在的V是："+maxV);
                                     opencv_test.opencv(input, output,0);
                                     System.out.println("按任意键继续，E停止");
                                     Scanner temp = new Scanner(System.in);
@@ -142,8 +142,8 @@ public class menu extends main
                                         String str = null;
                                         if(!"E".equals(str)||!"e".equals(str))
                                         {
-                                            lowL += 2;
-                                            System.out.println("现在的L是："+lowL);
+                                            lowH += 2;
+                                            System.out.println("现在的H是："+lowH);
                                             opencv_test.opencv(input, output,0);
                                             System.out.println("按任意键继续，E停止");
                                             Scanner temp = new Scanner(System.in);
@@ -162,8 +162,8 @@ public class menu extends main
                                         String str = null;
                                         if(!"E".equals(str)||!"e".equals(str))
                                         {
-                                            lowA += 2;
-                                            System.out.println("现在的A是："+lowA);
+                                            lowS += 2;
+                                            System.out.println("现在的S是："+lowS);
                                             opencv_test.opencv(input, output,0);
                                             System.out.println("按任意键继续，E停止");
                                             Scanner temp = new Scanner(System.in);
@@ -182,8 +182,8 @@ public class menu extends main
                                         String str = null;
                                         if(!"E".equals(str)||!"e".equals(str))
                                         {
-                                            lowB += 2;
-                                            System.out.println("现在的B是："+lowB);
+                                            lowV += 2;
+                                            System.out.println("现在的V是："+lowV);
                                             opencv_test.opencv(input, output,0);
                                             System.out.println("按任意键继续，E停止");
                                             Scanner temp = new Scanner(System.in);
@@ -211,10 +211,8 @@ public class menu extends main
                 break;
             case "T":
             case "t":
-                int x = 0;
                 Random r = new Random(1);
-                int times = 0;
-                while(times<100000)
+                while(test<100)
                 {
                     /*
                     lowL+=1;
@@ -222,36 +220,29 @@ public class menu extends main
                     System.out.println("目前的参数是："+lowL);
                      */
                     //玄学调试
-                    int ranlL = r.nextInt(100);
-                    int ranlA = r.nextInt(127);
-                    int ranlB = r.nextInt(127);
-                    int ranmL = r.nextInt(100);
-                    int ranmA = r.nextInt(127);
-                    int ranmB = r.nextInt(127);
+                    int ranlH = r.nextInt(10);
+                    int ranlS = r.nextInt(50);
+                    int ranlV = r.nextInt(60);
+                    int ranmH = r.nextInt(70);
+                    int ranmS = r.nextInt(255);
+                    int ranmV = r.nextInt(255);
 
-                    if (ranlL<ranmL&&ranlA<ranmA&&ranlB<ranmB) {
-                        x=x+1;
-                        lowL = ranlL;
-                        lowA = ranlA;
-                        lowB = ranlB;
-                        maxL = ranmL;
-                        maxA = ranmA;
-                        maxB = ranmB;
+                    if (ranlH<ranmH&&ranlS<ranmS&&ranlV<ranmV) {
+                        lowH = ranlH;
+                        lowS = ranlS;
+                        lowV = ranlV;
+                        maxH = ranmH;
+                        //maxA = ranmS;
+                        //maxB = ranmV;
                         opencv_test.opencv(input, output, 0);
-                        times+=1;
-                        System.out.println("已尝试"+times+"次");
-                        if (persentage>80)
-                        {
                             test += 1;
                             System.out.println("---序列" + test + "---");
-                            System.out.println("目前L的参数是：" + lowL);
-                            System.out.println("目前A的参数是：" + lowA);
-                            System.out.println("目前B的参数是：" + lowB);
-                            System.out.println("目前L的参数是：" + maxL);
-                            System.out.println("目前A的参数是：" + maxA);
-                            System.out.println("目前B的参数是：" + maxB);
-                            System.out.println("识别率为："+persentage+"%");
-                        }
+                            System.out.println("目前最小H的是：" + lowH);
+                            System.out.println("目前最小S的是：" + lowS);
+                            System.out.println("目前最小V的是：" + lowV);
+                            System.out.println("目前最小H的是：" + maxH);
+                            //System.out.println("目前最小S的是：" + maxS);
+                            //System.out.println("目前最小V的是：" + maxV);
                     }
                     //System.out.println("按任意键继续");
                     //Scanner temp = new Scanner(System.in);
